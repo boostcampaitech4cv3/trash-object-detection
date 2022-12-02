aec1f1c0cac64825294306b6765c84ebfcc661df
revanZX <revanZXSD@gmail.com>
2022-12-02 07:23:47 +0000

    ADD: convnext_restart

--- configs/_trashDet_/_base_/schedules/schedule_CosineRestart.py
+++ configs/_trashDet_/_base_/schedules/schedule_CosineRestart.py
@@ -1,10 +1,11 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None) # in boostcamp default : dict(grad_clip=dict(max_keep_ckpts=3, interval=1))  
 # learning policy
 lr_config = dict(
     policy='CosineRestart', 
-    periods=[20],
-    restart_weights=[1],
+    periods=[3900 * 2 for _ in range(10)],
+    restart_weights=[1 / (2 ** i) for i in range(10)],
+    by_epoch = False,
     min_lr=1e-07)
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+runner = dict(type='EpochBasedRunner', max_epochs=20)
--- configs/_trashDet_/convnext/cascade_mask_rcnn_convnext_xlarge_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in22k_aug3.py
+++ configs/_trashDet_/convnext/cascade_mask_rcnn_convnext_xlarge_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in22k_aug3.py
@@ -87,7 +87,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 optimizer = dict(constructor='LearningRateDecayOptimizerConstructor', _delete_=True, type='AdamW', 
-                 lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+                 lr=0.00015, betas=(0.9, 0.999), weight_decay=0.05,
                  paramwise_cfg={'decay_rate': 0.7,
                                 'decay_type': 'layer_wise',
                                 'num_layers': 12})
